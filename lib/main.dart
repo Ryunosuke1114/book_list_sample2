e33aeddd347437f2eafde11584cf5444d9194827
Ryunosuke1114 <ryunosuke1114@gmail.com>
2021-10-28 18:31:59 +0900

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:book_list_sample2/book_list/book_list_page2.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BookListSample',
+      home: BookListPage(),
+    );
+  }
+}
+
