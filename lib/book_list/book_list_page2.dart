6d23185172a22a007ffb6220d282787ecbb478b6
Ryunosuke1114 <ryunosuke1114@gmail.com>
2021-10-29 10:57:21 +0900

    firestoreに値を追加

--- lib/book_list/book_list_page2.dart
+++ lib/book_list/book_list_page2.dart
@@ -1,3 +1,4 @@
+import 'package:book_list_sample2/add_book/add_book_page.dart';
 import 'package:book_list_sample2/book_list/book_list_model.dart';
 import 'package:book_list_sample2/domain/book.dart';
 import 'package:flutter/material.dart';
@@ -35,10 +36,29 @@ class BookListPage extends StatelessWidget {
             );
           }),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+        floatingActionButton: Consumer<BookListModel>(builder: (context, model, child) {
+            return FloatingActionButton(
+              onPressed: () async {
+                //画面遷移
+              final bool? added = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddBookPage(),
+                    fullscreenDialog: true,
+                  ),
+                );
+              if (added != null && added){
+                final snackBar = SnackBar(
+                  backgroundColor: Colors.green,
+                  content: Text("本を追加しました"),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+              model.fetchBookList();
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            );
+          }
         ),
       ),
     );
